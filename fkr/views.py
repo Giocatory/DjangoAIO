e4b0e9a39ab9f7934ce23aa8cd5288af6722a06e
Giocatory <nivolyas@gmail.com>
2023-09-07 00:16:05 +0800

    06.09.2023

--- fkr/views.py
+++ fkr/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from DjangoAIO.settings import BASE_DIR
+import sqlite3
 
 
 def fkr(request):
-    return render(request, 'fkr.html')
+    # TMPRAB
+    connection = sqlite3.connect(f"{BASE_DIR / 'FKR.sqlite3'}")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM TMPRAB')
+    tmp_tuple = cursor.fetchall()
+
+    # sprrab
+    cursor.execute('SELECT * FROM sprrab')
+    sprrab_tuple = cursor.fetchall()
+
+    # sprdom
+    cursor.execute('SELECT * FROM sprdom')
+    sprdom_tuple = cursor.fetchall()
+
+    # Pays772
+    cursor.execute('SELECT * FROM Pays772')
+    Pays772_tuple = cursor.fetchall()
+    connection.close()
+
+    return render(request, 'fkr.html', {
+        'TMPRAB': tmp_tuple,
+        'sprrab': sprrab_tuple,
+        'sprdom': sprdom_tuple,
+        'Pays772': Pays772_tuple,
+    })
