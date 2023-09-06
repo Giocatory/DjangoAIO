a16b3f345f90dff2b758d7fda23687c9ab52e12b
Giocatory <nivolyas@gmail.com>
2023-09-06 22:38:21 +0800

    Initial commit

---
+++ eko/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def eko(request):
+    return render(request, 'eko.html')
---
+++ fkr/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def fkr(request):
+    return render(request, 'fkr.html')
