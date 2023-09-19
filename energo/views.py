115f26481b580be93d39e0d42b4b18338c922538
Giocatory <nivolyas@gmail.com>
2023-09-19 21:40:34 +0800

    19.09.2023

--- energo/views.py
+++ energo/views.py
@@ -5,4 +5,26 @@ import for_energo
 
 @lru_cache(maxsize=None)
 def energo(request):
+    # tables
+    html_table = []
+
+    # request from form
+    people_ls = request.POST.get("supplier-ls")
+    people_fam = request.POST.get("supplier-fam")
+    people_imia = request.POST.get("supplier-imia")
+
+    if people_ls == "" and people_fam == "" and people_imia == "":
+        html_table = "🐱‍‍🚀"
+
+    # Если введена только фамилия
+    elif people_ls == "" and people_fam != "" and people_imia == "":
+        pass
+    # Если введена фамилия и имя
+    elif people_ls == "" and people_fam != "" and people_imia != "":
+        pass
+
+    # Если введен лицевой счет
+    elif people_ls != "":
+        pass
+
     return render(request, 'energo.html')
